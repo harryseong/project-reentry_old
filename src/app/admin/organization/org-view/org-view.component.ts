8561dc06fc5a1eb7f67bf4dc2abb968ecf98346f
Harry Seong <harry@northwestern.edu>
2018-11-29 01:05:21 -0600

    Implemented Google maps in org view

--- src/app/admin/organization/org-view/org-view.component.ts
+++ src/app/admin/organization/org-view/org-view.component.ts
@@ -11,12 +11,12 @@ import {FirestoreService} from '../../../../shared/firestore/firestore.service';
 export class OrgViewComponent implements OnInit {
   orgName = '';
   org: any = null;
+  geocoder = new google.maps.Geocoder();
 
   constructor(private db: AngularFirestore, private firestoreService: FirestoreService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.orgName = this.route.snapshot.params['name'];
-
     const query = this.db.collection('organizations').ref.where('name', '==', this.orgName);
     query.get().then(querySnapshot => {
       if (querySnapshot.empty) {
@@ -24,9 +24,30 @@ export class OrgViewComponent implements OnInit {
       } else {
         querySnapshot.forEach(docSnapshot => {
           this.db.collection('organizations').doc(docSnapshot.id).ref.get().then(
-            org => this.org = org.data()
+            org => {
+              this.org = org.data();
+              const mapOption = {zoom: 15, mapTypeId: google.maps.MapTypeId.ROADMAP};
+              const map = new google.maps.Map(document.getElementById('gMap'), mapOption);
+              const oa = org.data().address;
+              const address = oa.streetAddress1 + ' ' + oa.streetAddress2 + ', ' + oa.city + ', ' + oa.state + ' ' + oa.zipCode;
+              this.codeAddress(address, map);
+            }
           );
         });
+      }
+    });
+  }
+
+  codeAddress(address: string, map) {
+    this.geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        const marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
       }
     });
   }
