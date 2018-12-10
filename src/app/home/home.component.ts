0761e30f447effbd6a59dcee27040cc5bb4c4304
Harry Seong <harry@northwestern.edu>
2018-12-10 14:32:44 -0600

    Org filters beginning to work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,13 +23,12 @@ export interface ServicesNearMeState {
   myLocationId: any;
   myLocation: any;
   serviceCategories: string[];
-  orgList: any[];
 }
 
 export interface SearchFilterControls {
   distanceRadius: number;
   includeReligiousOrgs: boolean;
-  includeOrgsWithEligibilityReqs: boolean;
+  noEligibilityRequirements: boolean;
   showOnlyOrgsWithTransport: boolean;
 }
 
@@ -56,6 +55,9 @@ export class HomeComponent implements OnInit {
   servicesNearMeState: ServicesNearMeState;
   searchFilterControls: SearchFilterControls;
   transition = '';
+  loading = false;
+  orgList: any[] = [];
+  filteredOrgList: any[] = [];
 
   constructor(private afAuth: AngularFireAuth, private firestoreService: FirestoreService, private userService: UserService,
               private zone: NgZone, private router: Router, private googleMapsService: GoogleMapsService) { }
@@ -64,17 +66,22 @@ export class HomeComponent implements OnInit {
     this.firestoreService.services.valueChanges()
       .subscribe(services => this.serviceList = this.firestoreService._sort(services, 'service'));
     this.transition = 'fadeIn';
-    this.servicesNearMeState = {display: false, loading: false, myLocationId: null, myLocation: null, serviceCategories: [], orgList: []};
-    this.searchFilterControls = {distanceRadius: 20, includeOrgsWithEligibilityReqs: true,
+    this.servicesNearMeState = {
+      display: false, loading: false, myLocationId: null, myLocation: null, serviceCategories: []};
+    this.searchFilterControls = {distanceRadius: 120, noEligibilityRequirements: true,
       includeReligiousOrgs: true, showOnlyOrgsWithTransport: false};
   }
 
   findServices() {
-    this.servicesNearMeState.loading = true;
+    this.loading = true;
     const address = this.servicesForm.get('location').value;
     this.codeAddress(address);
   }
 
+  /**
+   * This function will take the inputted city or address and turn it into
+   * @param address: city or address of user.
+   */
   codeAddress(address: string) {
     this.geocoder.geocode( { 'address': address}, (results, status) => {
       if (status.toString() === 'OK') {
@@ -86,47 +93,9 @@ export class HomeComponent implements OnInit {
           this.servicesNearMeState.myLocationId = results[0].place_id;
           this.servicesNearMeState.serviceCategories = this.servicesForm.get('services').value;
           this.servicesForm.reset();
-
-          // Get organizations and filter them here.
-          this.firestoreService.organizations.valueChanges().subscribe(rsp => {
-            const filteredOrgs = rsp.filter(org =>
-              org.services.some(service => this.servicesNearMeState.serviceCategories.includes(service)));
-            let orgCount = 0;
-
-            if (filteredOrgs.length > 0) {
-              filteredOrgs.forEach(org => {
-                this.googleMapsService.distanceMatrixService.getDistanceMatrix(
-                  {
-                    origins: [this.servicesNearMeState.myLocation],
-                    destinations: [org.address.gpsCoords],
-                    travelMode: google.maps.TravelMode.DRIVING,
-                    unitSystem: google.maps.UnitSystem.IMPERIAL
-                  }, (response, status2) => {
-                    if (status2.toString() === 'OK') {
-                      org['distance'] = response.rows[0].elements[0].distance.text;
-                      this.servicesNearMeState.orgList.push(org);
-                      orgCount++;
-                      console.log('Org Count: ' + orgCount);
-
-                      if (orgCount === filteredOrgs.length) {
-                        console.log('Org Count: ' + orgCount + ' READY!');
-                        const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
-                        this.servicesNearMeState.orgList = filteredOrgs.sort((a, b) => {
-                          return collator.compare(a.distance, b.distance);
-                        });
-                        this.servicesNearMeState.loading = false;
-                      }
-                    }
-                  }
-                );
-              });
-            } else {
-              console.log('Org Count: ' + orgCount + ' NO ORGS!!');
-              this.servicesNearMeState.loading = false;
-            }
-          });
+          this.getAndFilterOrgs();
         } else if (state !== 'MI') {
-          this.servicesNearMeState.loading = false;
+          this.loading = false;
           const message = 'The location provided was not found to be in Michigan. Please input a Michigan city or address.';
           const action = 'OK';
           this.zone.run(() => {
@@ -146,9 +115,75 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  /**
+   * Get orgs according to the user's choice of service categories.
+   */
+  getAndFilterOrgs() {
+    this.firestoreService.organizations.valueChanges().subscribe(rsp => {
+      const filteredOrgs = rsp.filter(org =>
+        org.services.some(service => this.servicesNearMeState.serviceCategories.includes(service)));
+      let orgCount = 0;
+
+      if (filteredOrgs.length > 0) {
+        filteredOrgs.forEach(org => {
+          this.googleMapsService.distanceMatrixService.getDistanceMatrix(
+            {
+              origins: [this.servicesNearMeState.myLocation],
+              destinations: [org.address.gpsCoords],
+              travelMode: google.maps.TravelMode.DRIVING,
+              unitSystem: google.maps.UnitSystem.IMPERIAL
+            }, (response, status2) => {
+              if (status2.toString() === 'OK') {
+                org['distance'] = response.rows[0].elements[0].distance.text;
+                this.orgList.push(org);
+                orgCount++;
+                console.log('Org Count: ' + orgCount);
+
+                if (orgCount === filteredOrgs.length) {
+                  console.log('Org Count: ' + orgCount + ' READY!');
+                  const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+                  // Sort orgs by distance.
+                  this.orgList = filteredOrgs.sort((a, b) => {
+                    return collator.compare(a.distance, b.distance);
+                  });
+                  this.filteredOrgList = Object.assign([], this.orgList);
+                  this.updateFilter();
+                  this.loading = false;
+                }
+              }
+            }
+          );
+        });
+      } else {
+        console.log('Org Count: ' + orgCount + ' NO ORGS!!');
+        this.loading = false;
+      }
+    });
+  }
+
+  updateFilter() {
+    this.filteredOrgList = Object.assign([], this.orgList);
+    this.filteredOrgList = this.filteredOrgList
+      .filter(org => org.distance.split(' mi')[0] <= this.searchFilterControls.distanceRadius);
+
+    if (this.searchFilterControls.includeReligiousOrgs === false) {
+      this.filteredOrgList = this.filteredOrgList
+        .filter(org => !org.services.includes('Religious Organization'));
+    }
+
+    if (this.searchFilterControls.noEligibilityRequirements === true) {
+      this.filteredOrgList = this.filteredOrgList
+        .filter(org => !org.eligibilityRequirements && !org.seniorRequirements);
+    }
+
+    if (this.searchFilterControls.showOnlyOrgsWithTransport === true) {
+      this.filteredOrgList = this.filteredOrgList.filter(org => org.transportation);
+    }
+  }
+
   back() {
-    this.servicesNearMeState = {display: false, loading: false, myLocationId: null, myLocation: null, serviceCategories: [], orgList: []};
-    this.searchFilterControls = {distanceRadius: 20, includeOrgsWithEligibilityReqs: true,
+    this.servicesNearMeState = {display: false, loading: false, myLocationId: null, myLocation: null, serviceCategories: []};
+    this.searchFilterControls = {distanceRadius: 120, noEligibilityRequirements: true,
       includeReligiousOrgs: true, showOnlyOrgsWithTransport: false};
   }
 }
